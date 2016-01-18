c08b2d32a4c8570aaf1d4daebd709b61e3dc24e0
Quinn Evans <heddwch@lambdaos.org>
2016-01-17 18:38:16 -0700

    Write initial implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,69 @@
+//! Z80 emulation library
+extern crate libc;
+mod z80e_core;
+use libc::{ c_void };
+
+/// An interface for a 16-bit addressed container of bytes.
+pub trait Z80Memory {
+    fn read_byte(&self, address: u16) -> u8;
+    fn write_byte(&self, address: u16, value: u8);
+}
+
+extern fn read_z80_memory<T: Z80Memory>(memory: *mut c_void, address: u16) -> u8 {
+    let memory: &mut T = unsafe { &mut *(memory as *mut T) };
+    memory.read_byte(address)
+}
+
+extern fn write_z80_memory<T: Z80Memory>(memory: *mut c_void, address: u16, value: u8) {
+    let memory: &mut T = unsafe { &mut *(memory as *mut T) };
+    memory.write_byte(address, value)
+}
+
+/// An interface for implementing one Z80 I/O port.
+pub trait Z80IODevice {
+    fn read_in(&self) -> u8;
+    fn write_out(&self, value: u8);
+}
+
+extern fn read_z80_device<T: Z80IODevice>(device: *mut c_void) -> u8 {
+    let device: &mut T = unsafe { &mut *(device as *mut T) };
+    device.read_in()
+}
+
+extern fn write_z80_device<T: Z80IODevice>(device: *mut c_void, value: u8) {
+    let device: &mut T = unsafe { &mut *(device as *mut T) };
+    device.write_out(value)
+}
+
+/// Z80 CPU implementation.
+pub struct Z80 {
+    core: *mut z80e_core::z80e_cpu,
+}
+
+impl Z80 {
+    /// Allocate a new Z80 core and install its memory.
+    pub fn new<T: Z80Memory>(memory: &mut T) -> Self {
+        unsafe {
+            let cpu = Z80 {
+                core: z80e_core::cpu_init(),
+            };
+            (*cpu.core).memory = (memory as *mut _) as *mut c_void;
+            (*cpu.core).read_byte = read_z80_memory::<T>;
+            (*cpu.core).write_byte = write_z80_memory::<T>;
+            cpu
+        }
+    }
+    /// Install a device on a Z80 I/O port.
+    pub fn install_device<T: Z80IODevice>(&self, port: u8, device: &mut T) {
+        let port: usize = port as usize;
+        unsafe {
+            (*self.core).devices[port].device = (device as *mut _) as *mut c_void;
+            (*self.core).devices[port].read_in = read_z80_device::<T>;
+            (*self.core).devices[port].write_out = write_z80_device::<T>;
+        }
+    }
+}
+
 #[test]
 fn it_works() {
 }
---
+++ src/z80e_core.rs
@@ -0,0 +1,60 @@
+//! Binding shim for libz80e-core
+extern crate libc;
+use self::libc::{ c_int, c_void, pthread_mutex_t };
+
+#[allow(dead_code)]
+#[repr(C)]
+pub struct z80e_registers {
+    af: u16,
+    bc: u16,
+    de: u16,
+    hl: u16,
+    _af: u16,
+    _bc: u16,
+    _de: u16,
+    _hl: u16,
+    pc: u16,
+    sp: u16,
+    ix: u16,
+    iy: u16,
+    i: u8,
+    r: u8,
+    wz: u16,
+}
+
+#[allow(dead_code)]
+#[repr(C)]
+pub struct z80e_io_device {
+    pub device: *mut c_void,
+    pub read_in: extern fn(*mut c_void) -> u8,
+    pub write_out: extern fn(*mut c_void, u8),
+}
+
+#[allow(dead_code)]
+#[repr(C)]
+pub struct z80e_cpu {
+    pub devices: [z80e_io_device; 100],
+    registers: z80e_registers,
+    iff1: bool,
+    iff2: bool,
+    int_mode: u8,
+    iff_wait: bool,
+    halted: bool,
+    prefix: u16,
+    pub memory: *mut c_void,
+    pub read_byte: extern fn(*mut c_void, u16) -> u8,
+    pub write_byte: extern fn(*mut c_void, u16, u8),
+    bus_lock: pthread_mutex_t,
+    interrupt: bool,
+    bus: u8,
+}
+
+#[allow(dead_code)]
+#[link(name = "z80e-core")]
+extern {
+    pub fn cpu_init() -> *mut z80e_cpu;
+    pub fn cpu_free(cpu: *mut z80e_cpu);
+    pub fn cpu_interrupt(cpu: *mut z80e_cpu, bus: u8) -> c_int;
+    pub fn cpu_try_interrupt(cpu: *mut z80e_cpu, bus: u8) -> c_int;
+    pub fn cpu_execute(cpu: *mut z80e_cpu, cycles: c_int) -> c_int;
+}
